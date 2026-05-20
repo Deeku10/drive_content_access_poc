9e7e5db91a5f8a95261458c8995b3ba7f53a3c80
deepak.vannivadivel <deepak.vannivadivel@subex.com>
2026-05-20 14:23:08 +0530

    Initial commit

---
+++ src/main/java/in/bydeepak/drive_content_access_poc/DriveContentAccessPocApplication.java
@@ -0,0 +1,13 @@
+package in.bydeepak.drive_content_access_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveContentAccessPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveContentAccessPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/bydeepak/drive_content_access_poc/DriveContentAccessPocApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bydeepak.drive_content_access_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveContentAccessPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
